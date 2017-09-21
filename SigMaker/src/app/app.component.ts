76efa555b9f2becf653c9f9640757f8f55d0240b
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-09-22 11:40:39 +1200

    SigMaker version request added

--- SigMaker/src/app/app.component.ts
+++ SigMaker/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import {Component} from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+  version:string = '';
+  sigMakerUrl:string = '';
+
+  constructor(httpClient: Http){
+    httpClient.request('https://api.github.com/repos/KEDRI-AUT/KEDRI-email-signature/releases/latest')
+      .subscribe(res => {this.version = res.json()['tag_name'], this.sigMakerUrl = res.json()['name']});
+  }
+
   contentCreated = {
     firstName: 'First name',
     lastName: 'Last name',
--- SigMaker/src/app/app.module.ts
+++ SigMaker/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SigComponent } from './sig/sig.component';
 import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
