93a8eb782e358070dbbbf780a1ba5f74e1da04d4
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-10-27 16:14:20 +1300

    on app start sig updates

--- SigMaker/src/app/app.component.ts
+++ SigMaker/src/app/app.component.ts
@@ -1,20 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Http} from "@angular/http";
+import {MessengerService} from "./messenger.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  content: {
+    firstName: string,
+    lastName: string,
+    email: string,
+    position: string,
+    phoneNumber: string,
+    mobileNumber: string
+  };
 
   version:string = '';
   sigMakerUrl:string = '';
 
-  constructor(httpClient: Http){
+  constructor(httpClient: Http, private messengerService: MessengerService){
     httpClient.request('https://api.github.com/repos/KEDRI-AUT/KEDRI-email-signature/releases/latest')
       .subscribe(res => {this.version = res.json()['tag_name'], this.sigMakerUrl = res.json()['name']});
   }
 
+  ngOnInit(){
+    this.content = this.messengerService.content;
   }
 }
