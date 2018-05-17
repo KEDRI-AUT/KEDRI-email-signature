819a868bd1c7c57a416d5e318a8e74b1870124e6
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-05-17 18:27:18 +1200

    code updates

--- SigMaker/src/app/sig/sig.component.ts
+++ SigMaker/src/app/sig/sig.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-sig',
   templateUrl: './sig.component.html',
   styleUrls: ['./sig.component.css']
 })
-
+export class SigComponent{
+  
   @Input() sigCompContent: {
     firstName: string,
     lastName: string,
